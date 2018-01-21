5c6eb2de038f93445e19d93dac7eec1892795b09
Cintia Tahirih <tahysluana@gmail.com>
2018-01-20 23:45:53 -0500

    añade seccion articulos

--- js/app.js
+++ js/app.js
@@ -1,6 +1,12 @@
 $(document).ready(function(){
-    $('.search-open').click(function(){
-        $('#search-toggle').toggle();
-        
-    });
+  $('.search-open').click(function(){
+    $('#search-toggle').toggle();
+  });
+  $('[data-id = "home-art"]').on('click', function() {
+    window.location.href = '../index.html';
+  });
+
+  $('[data-id = "more"]').on('click', function() {
+    window.location.href = 'views/articulos.html';
+  });
 });
